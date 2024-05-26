699b612e8f556a63dcfad0c91c6c331d988caf06
yjianzhu <yjianzhu@mail.ustc.edu.cn>
2024-05-26 21:58:19 +0800

    add something

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ if __name__=='__main__':
 
     # 定时
     
-    goods_shown = ["Xiaomi 14",'Redmi Note 13 5G','Redmi K70','Redmi Turbo 3']
+    goods_shown = ["Xiaomi 14","Xiaomi 13",'Redmi Note 13 5G',"Redmi Note 12 5G",'Redmi K70',"Redmi K60",'Redmi Turbo 3',"Redmi Note 12 Turbo"]
 
     st.title('小米商城评论数')
+    fig = plot_items(goods_shown)
+    st.pyplot(fig)
 
+    goods_shown = ["Xiaomi 14",'Redmi Note 13 5G','Redmi K70','Redmi Turbo 3']
     for item in goods_shown:
         st.subheader(item)
         fig = plot_daily_data_by_name(item)
--- utils/plot.py
+++ utils/plot.py
@@ -6,6 +6,16 @@ def create_chart(dates, daily_data, cumulative_data):
 
     # 柱状图：单日数据
     ax1.bar(dates, daily_data, color='b', label='Daily Data')
+    # 在每个柱条上显示数值
+    for bar in bars:
+        height = int(bar.get_height())
+        ax1.text(
+            bar.get_x() + bar.get_width() / 2,  # x 坐标
+            height,  # y 坐标
+            f'{height}', # 显示的文本是整数
+            ha='center',  # 水平对齐方式
+            va='bottom'  # 垂直对齐方式
+        )
     ax1.set_xlabel('Date')
     ax1.set_ylabel('Daily Data', color='b')
     ax1.tick_params(axis='y', labelcolor='b')
@@ -26,8 +36,8 @@ def plot_daily_data_by_name(name):
     all_data = query_comments_by_name(name)
     df = pandas.DataFrame(all_data, columns=['id', 'item_id', 'date', 'name', 'total_comments'])
     # 如果数据量太大，可以只取最近的 30 天
-    if len(df) > 30:
-        df = df[-30:]
+    if len(df) > 10:
+        df = df[-10:]
 
     # 绘制双轴图，横轴为日期，左轴为单日数据，右轴为累计数据
     dates = df['date']
@@ -35,7 +45,18 @@ def plot_daily_data_by_name(name):
     cumulative_data = df['total_comments']
 
     fig, ax1 = plt.subplots()
-    ax1.bar(dates, daily_data, color='r', label='Daily Data')
+    bars = ax1.bar(dates, daily_data, color='r', label='Daily Data')
+    # 在每个柱条上显示数值
+    for bar in bars:
+        height = bar.get_height()
+        ax1.text(
+            bar.get_x() + bar.get_width() / 2,  # x 坐标
+            height,  # y 坐标
+            f'{height}',  # 显示的文本
+            ha='center',  # 水平对齐方式
+            va='bottom'  # 垂直对齐方式
+        )
+
     ax1.set_xlabel('Date')
     # 日期旋转 45 度
     plt.xticks(rotation=45)
@@ -50,4 +71,41 @@ def plot_daily_data_by_name(name):
     fig.legend(loc='upper left', bbox_to_anchor=(0.2,0.9))
     plt.tight_layout()
     
+    return fig
+
+def plot_items(names):
+    """绘制多个商品的评论数图表，柱状图，横坐标为名称，纵坐标为评论数，title是最新日期"""
+    counts = []
+
+    for name in names:
+        all_data = query_comments_by_name(name)
+        df = pandas.DataFrame(all_data, columns=['id', 'item_id', 'date', 'name', 'total_comments'])
+        # 向 counts 中添加最新的评论数
+        if len(df) == 0:
+            counts.append(0)
+        else:
+            counts.append(df['total_comments'].iloc[-1])
+    
+    # 绘制柱状图
+    fig, ax = plt.subplots()
+    bars = ax.bar(names, counts)
+    ax.set_xlabel('Name')
+    ax.set_ylabel('Total Comments')
+
+    # 在每个柱条上显示数值
+    for bar in bars:
+        height = bar.get_height()
+        ax.text(
+            bar.get_x() + bar.get_width() / 2,  # x 坐标
+            height,  # y 坐标
+            f'{height}',  # 显示的文本
+            ha='center',  # 水平对齐方式
+            va='bottom'  # 垂直对齐方式
+        )
+    try:
+        ax.set_title(df['date'].iloc[-1])
+    except:
+        pass
+    plt.xticks(rotation=45)
+    plt.tight_layout()
     return fig
